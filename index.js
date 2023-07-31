332813e5d8e1f678094ee7e818e426ce4f805291
chenchaiah98 <rchenchaiah98@gmail.com>
2023-07-31 15:22:41 +0000

    added files and code

---
+++ index.js
@@ -0,0 +1,24 @@
+const element = (
+  // Write your code here.
+  <div class="background">
+    <h1>congratulations</h1>
+    <div class="card">
+      <div>
+        <img
+          src="https://assets.ccbp.in/frontend/react-js/congrats-card-profile-img.png"
+          alt="image"
+        />
+        <h2>name</h2>
+        <p>text paragraph</p>
+      </div>
+      <div>
+        <img
+          src="https://assets.ccbp.in/frontend/react-js/congrats-card-watch-img.png"
+          alt="image"
+        />
+      </div>
+    </div>
+  </div>
+);
+
+ReactDOM.render(element, document.getElementById("root"));
